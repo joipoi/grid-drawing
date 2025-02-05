9ecc564be725618be5145b612ec03642c4e469d5
joel <joel.ekne@skaggbyran.se>
2025-02-05 12:12:20 +0100

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //Grid settings
 const transitionTime = 1000;
-const squareDelay = 100;
-let gridWidth = 10;
-let gridHeight = 10;
-let squareSize = 80;
-let showNumbers = true;
-let customKeyBind = 'a';
+let gridWidth = 20;
+let gridHeight = 20;
+let squareSize = 40;
+let showNumbers = false;
+let color1 = 'black';
+let color2 = 'orange';
+let drawSpeed = 50;
 
 //Javascript variables
 let currentPattern = [];
@@ -13,12 +14,13 @@ let preDefinedDrawings = {
     Smiley: [11, 12, 22, 21, 17, 27, 26, 16, 52, 62, 63, 64, 74, 75, 76, 77, 67, 57, 58],
     Loop: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 89, 79, 69, 59, 49, 39, 29, 19, 9, 8, 7, 6, 5, 4, 3, 2, 1, 11, 21, 31, 41, 51, 61, 71, 81, 82, 83, 84, 85, 86, 87, 88, 78, 68, 58, 48, 38, 28, 18, 17, 16, 15, 14, 13, 12, 22, 32, 42, 52, 62, 72, 73, 74, 75, 76, 77, 67, 57, 47, 37, 27, 26, 25, 24, 23, 33, 43, 53, 63, 64, 65, 66, 56, 46, 36, 35, 34, 44, 54, 55, 45]
 }
-let drawingEnabled = false;
+let drawingEnabled = true;
 
 
 //Misc Elems
 const container = document.getElementById('squareContainer');
 const patternStringOutput = document.getElementById('patternStringOutput');
+const r = document.querySelector(':root');
 
 //Main Buttons Elems
 const drawBtn = document.getElementById('drawBtn');
@@ -31,8 +33,10 @@ const widthInput = document.getElementById('widthInput');
 const heightInput = document.getElementById('heightInput');
 const showNumsCB = document.getElementById('showNumsCB');
 const tempCB = document.getElementById('tempCB');
-const pauseKeybindInput = document.getElementById('pauseKeybindInput');
 const holdDrawCB = document.getElementById('holdDrawCB');
+const color1Input = document.getElementById('color1Input');
+const color2Input = document.getElementById('color2Input');
+const drawingTimeInput = document.getElementById('drawingTimeInput');
 
 //Predefined Drawing elems
 const selectDrawBtn = document.getElementById('selectDrawBtn');
@@ -56,16 +60,15 @@ function init(){
     resetBoardBtn.addEventListener('click', () => {
         const squares = document.querySelectorAll('.square');
         squares.forEach((square) => {
-            square.style.backgroundColor = 'black'; 
+            square.style.backgroundColor = color1; 
         });
         const numbers = document.querySelectorAll('.number');
         numbers.forEach((square) => {
-            square.style.color = 'black'; 
+            square.style.color = color1; 
         });
     });
  
     //Drawing Settings elems
-    const r = document.querySelector(':root');
 
     widthInput.addEventListener('change', (e) => {
         gridWidth = e.target.value;
@@ -89,7 +92,7 @@ function init(){
         const squares = document.querySelectorAll('.square');
         squares.forEach((square) => {
             let childElem = square.children[0];
-            if (!e.target.checked) {
+            if (e.target.checked) {
                 childElem.style.display = "inline";
                 showNumbers = true;
               } else {
@@ -99,23 +102,38 @@ function init(){
         });
     });
 
-    pauseKeybindInput.addEventListener('change', (e) => {
-        const keyBind = e.target.value;
+    const mouseDownHandler = (event) => {
+        if (event.button === 0) {
+            drawingEnabled = true;
+        }
+    };
     
-        // Update the custom keybind and ensure it's a single character
-        if (keyBind.length === 1) {
-            customKeyBind = keyBind;
+    const mouseUpHandler = () => {
+        drawingEnabled = false;
+    };
+
+    holdDrawCB.addEventListener('change', (e) => {
+        if (e.target.checked) {
+            document.addEventListener('mousedown', mouseDownHandler);
+            document.addEventListener('mouseup', mouseUpHandler);
         } else {
-            alert('Please enter a single character for the keybind.');
+            // Remove the event listeners when not checked
+            document.removeEventListener('mousedown', mouseDownHandler);
+            document.removeEventListener('mouseup', mouseUpHandler);
         }
     });
 
-    holdDrawCB.addEventListener('change', (e) => {
-        if(e.target.checked){
+    color1Input.addEventListener('change', (e) => {
+        color1 = e.target.value;
+        r.style.setProperty('--color1', color1);
+    });
+    color2Input.addEventListener('change', (e) => {
+        color2 = e.target.value;
+        r.style.setProperty('--color2', color2);
+    });
 
-        }else{
-            
-        }
+    drawingTimeInput.addEventListener('change', (e) => {
+        drawSpeed = e.target.value;
     });
 
     //Predefined Drawing elems
@@ -136,21 +154,15 @@ function init(){
 
     //other
 
-    document.addEventListener('mousedown', (event) => {
-        if (event.button === 0) {
-            drawingEnabled = true;
-        }
+   
+
+    document.addEventListener('selectstart', (e) => {
+        e.preventDefault(); // Prevents text selection
     });
 
-    document.addEventListener('mouseup', () => {
-        drawingEnabled = false;
-    });
-
- 
-    document.addEventListener('keydown', handleKeyDown);
-    document.addEventListener('keyup', handleKeyUp);
     createSquares();
     dropDownLogic();
+    initDrawing();
 }
 init();
 function createSquares() {
@@ -163,24 +175,6 @@ function createSquares() {
         squareNum.innerHTML = i;
         squareNum.style.display = showNumbers ? "inline" : "none";
         square.append(squareNum);
-        let revertTimeout;
-
-        square.addEventListener('mouseenter', () => {
-            clearTimeout(revertTimeout); 
-            if(drawingEnabled){
-            square.style.backgroundColor = 'orange'; 
-            patternStringOutput.innerHTML += "<span class='number'>"+ i +", </span>";
-            currentPattern.push(i);
-        }
-        });
-
-        square.addEventListener('mouseleave', () => {
-            if (!tempCB.checked && drawingEnabled) {
-                revertTimeout = setTimeout(() => {
-                    square.style.backgroundColor = 'black'; 
-                }, transitionTime); 
-            }
-        });
 
         container.appendChild(square);
     }
@@ -191,22 +185,23 @@ function drawPattern(pattern, isTemporary, includeNumbers) {
     const squares = document.querySelectorAll('.square');
     const numbers = document.querySelectorAll('.number');
     let delay = 0;
+    
 
     pattern.forEach((index, count) => {     
-        delay += squareDelay; 
+        delay += (150 - drawSpeed); 
         
         setTimeout(() => {
-            squares[index].style.backgroundColor = 'orange'; 
+            squares[index].style.backgroundColor = color2; 
             if(includeNumbers){
-                numbers[count].style.color = 'orange'; 
+                numbers[count].style.color = color2; 
             }
             
             
             if (isTemporary) {
                 setTimeout(() => {
-                    squares[index].style.backgroundColor = 'black'; 
+                    squares[index].style.backgroundColor = color1; 
                     if(includeNumbers){
-                    numbers[count].style.color = 'black'; 
+                    numbers[count].style.color = color1; 
                     }
                 }, transitionTime); 
             }
@@ -216,31 +211,60 @@ function drawPattern(pattern, isTemporary, includeNumbers) {
 
 
 function dropDownLogic() {
-    const settings1Btn = document.getElementById('settings1Btn');
-    settings1Btn.addEventListener('click', () => {
-        document.getElementById("settings1Div").classList.toggle("show");
-    });
-    const settings2Btn = document.getElementById('settings2Btn');
-    settings2Btn.addEventListener('click', () => {
-        document.getElementById("settings2Div").classList.toggle("show");
-    });
+    const dropDownContainer = document.getElementById('dropDownContainer');
+    
+    const toggleDropdown = (btn, contentId) => {
+        const content = document.getElementById(contentId);
+        
+        // Close other dropdowns
+        const allDropdowns = ['settings1Div', 'settings2Div', 'settings3Div'];
+        allDropdowns.forEach((id) => {
+            if (id !== contentId) {
+                document.getElementById(id).classList.remove('show');
+            }
+        });
+        
+        // Toggle the clicked dropdown
+        content.classList.toggle('show');
+    };
 
+    const settings1Btn = document.getElementById('settings1Btn');
+    settings1Btn.addEventListener('click', () => toggleDropdown(settings1Btn, 'settings1Div'));
+    
+    const settings2Btn = document.getElementById('settings2Btn');
+    settings2Btn.addEventListener('click', () => toggleDropdown(settings2Btn, 'settings2Div'));
+    
     const settings3Btn = document.getElementById('settings3Btn');
-    settings3Btn.addEventListener('click', () => {
-        document.getElementById("settings3Div").classList.toggle("show");
-    });
-    
-    
+    settings3Btn.addEventListener('click', () => toggleDropdown(settings3Btn, 'settings3Div'));
 }
 
-function handleKeyDown(event){
-    if (event.key.toLowerCase() === customKeyBind.toLowerCase()) {
-        drawingEnabled = false;
-    }
-};
+function initDrawing() {
+    let revertTimeout;
+    const changedSquares = new Set(); 
 
-function handleKeyUp(event){
-    if (event.key.toLowerCase() === customKeyBind.toLowerCase()) {
-        drawingEnabled = true;
-    }
-};
+    container.addEventListener('mouseover', (e) => {
+        if (e.target.classList.contains('square')) {
+            clearTimeout(revertTimeout);
+
+            if (drawingEnabled) {
+                const square = e.target;
+                const index = Array.from(container.children).indexOf(square);
+                
+                square.style.backgroundColor = color2;
+                patternStringOutput.innerHTML += `<span class='number'>${index}, </span>`;
+                currentPattern.push(index);
+
+                changedSquares.add(square);
+
+            }
+            if (!tempCB.checked) {
+                revertTimeout = setTimeout(() => {
+                    changedSquares.forEach((s) => {
+                        s.style.backgroundColor = color1; 
+                    });
+                    changedSquares.clear(); 
+                }, transitionTime);
+            }
+        }
+    });
+}
